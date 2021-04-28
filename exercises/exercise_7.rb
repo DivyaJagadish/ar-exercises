4bf9c45075eb37887b2d085a4e57c36d209c95ec
Divya Jagadish <divyajagadish21@gmail.com>
2021-04-28 00:03:10 +0000

    Exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,22 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+class Employee 
+  validates :first_name ,:last_name ,presence: true
+  validates :hourly_rate , :inclusion => 40..200 ,numericality: { only_integer: true }
+  validates :store , presence: true
+end
+class Store
+  validates :name , length:{minimum: 3}
+  validates :annual_revenue ,numericality: { only_integer: true}
+  validates :womens_apparel ,presence:true , unless: :mens_apparel
+  validates :mens_apparel ,presence:true, unless: :womens_apparel
+end
+puts "Enter a Store name"
+@storename = gets.chomp
+@newstore =Store.create(name: @storename)
+#  checks validity
+@newstore.valid?
+# prints messages
+p @newstore.errors.full_messages
+
